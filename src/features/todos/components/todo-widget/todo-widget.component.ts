9284b2b4de9ec712dcd9cd4bfb79f1e42a597bac
tsm <attila.egyed.91@gmail.com>
2016-08-12 00:56:22 +0200

    -

--- src/features/todos/components/todo-widget/todo-widget.component.ts
+++ src/features/todos/components/todo-widget/todo-widget.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { TodoEffects } from '../../effects';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import { mergeEffects } from '@ngrx/effects';
 
 @Component({
@@ -14,12 +15,19 @@ import { mergeEffects } from '@ngrx/effects';
   `,
   styles: [require('./todo-widget.component.scss')]
 })
-export class TodoWidgetComponent {
+export class TodoWidgetComponent implements OnDestroy {
+  subscription: Subscription;
+
   constructor(private todoEffects: TodoEffects, private store: Store<any>) {
     this.injectEffects();
   }
 
   injectEffects() {
-    mergeEffects(this.todoEffects).subscribe(this.store);
+    this.subscription = mergeEffects(this.todoEffects).subscribe(this.store);
   }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
 }
